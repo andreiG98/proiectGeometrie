e00949136d2eeae90b66f1549f273b4a72f35a51
Andrei Gidea <45100135+andreiG98@users.noreply.github.com>
2018-12-21 21:31:52 +0200

    Add files via upload

--- intersectieSegmente.h
+++ intersectieSegmente.h
@@ -1,3 +1,199 @@
+<<<<<<< HEAD
+#ifndef INTERSECTIESEGMENTE_H_INCLUDED
+#define INTERSECTIESEGMENTE_H_INCLUDED
+#include <algorithm>
+using namespace std;
+struct Point {
+    float x;
+    float y;
+    int indice;
+};
+float det(float a1, float a2, float a3, float a4)
+{
+    return 1.0*(a1*a4-a2*a3);
+}
+
+int intersect(float &a, float &b, float c, float d)
+{
+    if (d<a || b<c)
+    {
+        return -1;
+    }
+    if (b==c)
+    {
+        a=c;
+        return 0;
+    }
+    if (c<b && b<d)
+    {
+        if (c<=a)
+            return 0;
+        if (c>a)
+        {
+            a=c;
+            return 0;
+        }
+    }
+    if (b==d)
+    {
+        if (a<c)
+        {
+            a=c;
+            return 0;
+        }
+    }
+    if (b>d)
+    {
+        if (a<c)
+        {
+            a=c;
+        }
+        b=d;
+        return 0;
+    }
+    return 0;
+}
+bool verificareIntersectieSegmente(Point A, Point B, Point C, Point D)
+{
+    float delta=det( A.x-B.x, D.x-C.x,
+                      A.y-B.y, D.y-C.y);
+    float lambda,miu,lambda_min,lambda_max;
+    if (delta!=0.0)
+    {
+        //se intersecteaza
+        lambda=det( D.x-B.x, D.x-C.x,
+                 D.y-B.y, D.y-C.y)*1.0/delta;
+        miu=det( A.x-B.x, D.x-B.x,
+                 A.y-B.y, D.y-B.y)*1.0/delta;
+        if (0 <= lambda && lambda <= 1 && 0 <= miu && miu <= 1)
+        {
+            //M.x=lambda*A.x+(1-lambda)*B.x;
+            //M.y=lambda*A.y+(1-lambda)*B.y;
+            //fout<<"Segmentele se intersecteaza in punctul "<<M.x<<' '<<M.y<<'\n';
+            return true;
+        }
+        else
+        {
+            //dreptele determinate se intersecteaza, dar segmentele nu
+            //fout<<"Segmentele nu se intersecteaza\n";
+            return false;
+        }
+
+    }
+    else
+    {
+        if ( det(A.x-B.x, D.x-B.x,
+                 A.y-B.y, D.y-B.y)==0.0 )
+        {
+            //sistem compatibil nedeterminat
+            if (A.x==B.x)
+            {
+                //lambda apartine [0,1]
+                if (C.x==D.x)
+                {
+                    if (A.y==B.y && C.y==D.y)
+                    {
+                        //Segmentele coincid
+                        //fout<<"Intersectia este segmentul "<<A.x<<' '<<A.y<<" - "<<B.x<<' '<<B.y<<'\n';
+                        return true;
+                    }
+                    else
+                        if (A.y==B.y)
+                        {
+                            //A=B
+                            //fout<<"Segmentele se intersecteaza in punctul "<<A.x<<' '<<A.y<<'\n';
+                            return true;
+                        }
+                        else
+                            if (C.y==D.y)
+                            {
+                                //C=D
+                                //fout<<"Segmentele se intersecteaza in punctul "<<C.x<<' '<<C.y<<'\n';
+                                return true;
+                            }
+                            else
+                            {
+                                lambda_min=min((D.y-B.y+C.y-D.y)*1.0/(A.y-B.y),(D.y-B.y)*1.0/(A.y-B.y));
+                                lambda_max=max((D.y-B.y+C.y-D.y)*1.0/(A.y-B.y),(D.y-B.y)*1.0/(A.y-B.y));
+                                if (intersect(lambda_min,lambda_max,0,1)==-1)
+                                {
+                                    //fout<<"Segmentele nu se intersecteaza\n";
+                                    return false;
+                                }
+                                else
+                                {
+                                    if (lambda_min==lambda_max)
+                                    {
+                                        //M.x=lambda_min*A.x+(1-lambda_min)*B.x;
+                                        //M.y=lambda_min*A.y+(1-lambda_min)*B.y;
+                                        //fout<<"Segmentele se intersecteaza in punctul "<<M.x<<' '<<M.y<<'\n';
+                                        return true;
+                                    }
+                                    else
+                                    {
+                                        //M.x=lambda_min*A.x+(1-lambda_min)*B.x;
+                                        //M.y=lambda_min*A.y+(1-lambda_min)*B.y;
+
+                                        //N.x=lambda_max*A.x+(1-lambda_max)*B.x;
+                                        //N.y=lambda_max*A.y+(1-lambda_max)*B.y;
+                                        //fout<<"Intersectia este segmentul "<<M.x<<' '<<M.y<<" - "<<N.x<<' '<<N.y<<'\n';return true;
+                                        return true;
+                                    }
+                                }
+                            }
+                }
+                else
+                {
+                    miu=(D.x-B.x)*1.0/(D.x-C.x);
+                    //intersectia e segmentul AB,unde A=B
+                    //M.x=miu*C.x+(1-miu)*D.x;
+                    //M.y=miu*C.y+(1-miu)*D.y;
+                    //fout<<"Intersectia este punctul "<<M.x<<' '<<M.y<<'\n';
+                    return true;
+                }
+            }
+            else
+            {
+                lambda_min=min((D.x-B.x+C.x-D.x)*1.0/(A.x-B.x),(D.x-B.x)*1.0/(A.x-B.x));
+                lambda_max=max((D.x-B.x+C.x-D.x)*1.0/(A.x-B.x),(D.x-B.x)*1.0/(A.x-B.x));
+                if (intersect(lambda_min,lambda_max,0,1)==-1)
+                {
+                    //fout<<"Segmentele nu se intersecteaza\n";
+                    return false;
+                }
+                else
+                {
+                    if (lambda_min==lambda_max)
+                    {
+                        //M.x=lambda_min*A.x+(1-lambda_min)*B.x;
+                        //M.y=lambda_min*A.y+(1-lambda_min)*B.y;
+                        //fout<<"Segmentele se intersecteaza in punctul "<<M.x<<' '<<M.y<<'\n';
+                        return true;
+                    }
+                    else
+                    {
+                        //M.x=lambda_min*A.x+(1-lambda_min)*B.x;
+                        //M.y=lambda_min*A.y+(1-lambda_min)*B.y;
+
+                        //N.x=lambda_max*A.x+(1-lambda_max)*B.x;
+                        //N.y=lambda_max*A.y+(1-lambda_max)*B.y;
+                        //fout<<"Intersectia este segmentul "<<M.x<<' '<<M.y<<" - "<<N.x<<' '<<N.y<<'\n';
+                        return true;
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            //nu se intersecteaza
+            return false;
+        }
+    }
+}
+
+#endif // INTERSECTIESEGMENTE_H_INCLUDED
+=======
 #ifndef INTERSECTIESEGMENTE_H_INCLUDED
 #define INTERSECTIESEGMENTE_H_INCLUDED
 
@@ -103,3 +299,4 @@ bool verificareIntersectieSegmente(Point A1, Point A2, Point A3, Point A4) //1 -
 }
 
 #endif // INTERSECTIESEGMENTE_H_INCLUDED
+>>>>>>> 3977c97ffabbb27e50556d033119547c7f2948e7
--- main.cpp
+++ main.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <deque>
 #include <algorithm>
+#include <cstring>
+#include "intersectieSegmente.h"
 #define NMAX 2000
 
 using namespace std;
 
-struct Point {
+/*struct Point {
     float x;
     float y;
     int indice;
+};*/
+
+struct Triunghi {
+    int punct1;
+    int punct2;
+    int punct3;
 };
 
 bool convex[NMAX];
 bool ear[NMAX];
+//bool Diag[NMAX][NMAX];
+bool viz[NMAX];
+bool vizibile[NMAX];
 
 int head;
 
 vector <Point> poligon;
 vector <Point> auxiliar;
 vector <Point> stackk;
+deque <Point> lista;
+vector <Triunghi> triunghiuri;
+vector <Triunghi> triunghiuriVizibile;
+Point punctDeVerificat;
 
 void read() {
     ifstream fin("date.in");
-    //int n;
     float x, y;
     int i = 0;
-    //fin >> n;
-    //for(int i = 0; i < n; i++) {
+    fin >> punctDeVerificat.x >> punctDeVerificat.y;
     while(fin >> x >> y){
-        //fin >> x >> y;
         poligon.push_back({x, y, i});
         i++;
     }
@@ -59,11 +72,14 @@ void sort_points() {
 
 void verificareVfConvexe() {
     int i;
-    for(i = 0; i < poligon.size(); i++) {
-        auxiliar.push_back(poligon[i]);
+    deque<Point>::iterator pCurrent;
+    for(pCurrent = lista.begin(); pCurrent != lista.end(); pCurrent++) {
+        auxiliar.push_back(*pCurrent);
+    }
+    for (i = 0; i < poligon.size(); i++) {
         convex[i] = true;
     }
-    sort_points();
+    //sort_points();
     stackk.push_back(auxiliar[0]);
     stackk.push_back(auxiliar[1]);
     head = 2;
@@ -78,19 +94,165 @@ void verificareVfConvexe() {
         stackk.push_back(auxiliar[i]);
         ++head;
     }
+    stackk.clear();
     auxiliar.clear();
 }
 
-int main()
-{
-    read();
-    verificareVfConvexe();
+/*void diagonale() {
+    int i, j, k, prevI, nextJ;
+    bool ok;
+    for (i = 0; i < poligon.size(); i++)
+        for (j = i + 2; j < poligon.size(); j++) {
+            ok = true;
+            prevI = i - 1;
+            if (prevI == -1)
+                prevI = poligon.size() - 1;
+            nextJ = j + 1;
+            if (nextJ == poligon.size())
+                nextJ = 0;
+            for (k = 1; k < poligon.size() && ok; k++) {
+                if (verificareIntersectieSegmente(poligon[i], poligon[j], poligon[k-1], poligon[k]));
+                    ok = false;
+            }
+            if (ok)
+                Diag[i][j] = Diag[j][i] = true;
+        }
+}*/
 
-    /*sort_points();
-    for(int i = 0; i < poligon.size(); i++)
-        cout<<poligon[i].x<<' '<<poligon[i].y<<endl;*/
-    //cout<<testOrientare(poligon[10], poligon[0], poligon[1]);
-    for(int i = 0; i < poligon.size(); i++)
-        cout << convex[i] <<' ';
+deque<Point>::iterator findEar() {
+    deque<Point>::iterator pNext, pPrev, pCurrent;
+    bool found = false;
+    for (pCurrent = lista.begin(); pCurrent != lista.end() && (!found); pCurrent++)
+        if (convex[pCurrent->indice] && viz[pCurrent->indice] == false) {
+            /*pPrev = pCurrent - 1;
+            if (pCurrent == lista.begin())
+                pPrev = lista.end() - 1;
+            pNext = pCurrent + 1;
+            if (pNext == lista.end())
+                pNext = lista.begin();
+            if (convex[pNext->indice] && convex[pPrev->indice]) {
+                found = true;
+            }
+            */
+            found = true;
+        }
+    if (found)
+        return (pCurrent - 1);
+    return lista.end();
+}
+
+void triangulare() {
+    int i, j, stop;
+    bool ok;
+    for (i = 0; i < poligon.size(); i++)
+        lista.push_back(poligon[i]);
+    deque<Point>::iterator pNext, pPrev, pCurrent, pDiagonal;
+    while (lista.size() > 4) {
+        verificareVfConvexe();
+        pCurrent = findEar();
+        pPrev = pCurrent - 1;
+        if (pCurrent == lista.begin())
+            pPrev = lista.end() - 1;
+        pNext = pCurrent + 1;
+        if (pNext == lista.end())
+            pNext = lista.begin();
+        ok = true;
+        stop = pPrev->indice - 1;
+        if (stop < 0)
+            stop = stop + poligon.size() + 1;
+        for (j = pNext->indice + 1; j != stop && ok; j++) {
+            if (j == poligon.size())
+                j = 0;
+            if (verificareIntersectieSegmente(poligon[pPrev->indice], poligon[pNext->indice], poligon[j], poligon[j + 1]))
+                ok = false;
+        }
+
+        if (ok) {
+            triunghiuri.push_back({pPrev->indice, pCurrent->indice, pNext->indice});
+            lista.erase(pCurrent);
+            memset(viz, 0, sizeof(viz));
+        }
+        else {
+            viz[pCurrent->indice] = true;
+        }
+    }
+    // cand avem doar 4 puncte ramase
+    ok = true;
+    for (pCurrent = lista.begin(); pCurrent != lista.end() && ok; pCurrent++)
+        if (!convex[pCurrent->indice])
+            ok = false;
+    if (ok)
+    {
+        // poligon convex
+        pCurrent = lista.begin();
+        pNext = pCurrent + 1;
+        pDiagonal = pCurrent + 2;
+        pPrev = pCurrent + 3;
+    }
+    else
+    {
+        pCurrent--;
+        // pCurrent - varf concav
+        pNext = pCurrent + 1;
+        if (pNext >= lista.end())
+           pNext = lista.begin() + (pNext - lista.end());
+        pDiagonal = pCurrent + 2;
+        if (pDiagonal >= lista.end())
+           pDiagonal = lista.begin() + (pDiagonal - lista.end());
+        pPrev = pCurrent + 3;
+        if (pPrev >= lista.end())
+           pPrev = lista.begin() + (pPrev - lista.end());
+    }
+    triunghiuri.push_back({pCurrent->indice, pNext->indice, pDiagonal->indice});
+    triunghiuri.push_back({pDiagonal->indice, pPrev->indice, pCurrent->indice});
+}
+
+void puncteVizibile() {
+    int i, j, stop;
+    bool ok;
+    Point pPrev, pCurent, pNext;
+    for (i = 0; i < poligon.size(); i++) {
+        pCurent = poligon[i];
+        pPrev = poligon[i - 1];
+        if(i == 0)
+            pPrev = poligon[poligon.size() - 1];
+        pNext = poligon[i + 1];
+        if(i == poligon.size() - 1)
+            pNext = poligon[0];
+        stop = pPrev.indice - 1;
+        if(pPrev.indice == 0)
+            stop = poligon[poligon.size() - 1].indice;
+        ok = true;
+        for(j = pNext.indice; j != stop + 1 && ok; j++) {
+            if (j == poligon.size())
+                j = 0;
+            if (verificareIntersectieSegmente(punctDeVerificat, pCurent, poligon[j], poligon[j + 1]))
+                ok = false;
+        }
+        vizibile[i] = ok;
+    }
+}
+
+void triunghiuriVizibileDinPc() {
+    puncteVizibile();
+    for (int i = 0; i < triunghiuri.size(); i++) {
+        if(vizibile[triunghiuri[i].punct1] && vizibile[triunghiuri[i].punct2] && vizibile[triunghiuri[i].punct3]) {
+            triunghiuriVizibile.push_back({triunghiuri[i].punct1, triunghiuri[i].punct2, triunghiuri[i].punct3});
+        }
+    }
+}
+
+int main() {
+    read();
+    //diagonale();
+    triangulare();
+    /*for (int i = 0; i < triunghiuri.size(); i++)
+        cout << triunghiuri[i].punct1 << ' ' << triunghiuri[i].punct2 << ' ' << triunghiuri[i].punct3 << '\n';
+    for (int i = 0; i < poligon.size(); i++)
+        cout<<vizibile[i]<<' ';
+    cout<<endl;*/
+    triunghiuriVizibileDinPc();
+    for (int i = 0; i < triunghiuriVizibile.size(); i++)
+        cout << triunghiuriVizibile[i].punct1 << ' ' << triunghiuriVizibile[i].punct2 << ' ' << triunghiuriVizibile[i].punct3 << '\n';
     return 0;
 }
